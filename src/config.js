b8a4cab0c893b1dcd88d6800b5438ee55ecc842f
ram <reddylavanuru315@gmail.com>
2025-02-10 14:09:57 +0530

    correted

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const produtsServiceLocalHost = 'http://localhost:8132/api'
 const usersServiceLocalHost = 'http://localhost:8232/api'
 
-const produtsServiceDevHost = 'http://35.237.220.134/:5132/api'
-const usersServiceDevHost = 'http://35.237.220.134/:5232/api'
+const produtsServiceDevHost = 'http://35.237.220.134:5132/api'
+const usersServiceDevHost = 'http://35.237.220.134:5232/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
